9b4bb5272abb18e422f1e831f3e8a72a6937884c
awwdev <aw.mail.ranked@gmail.com>
2020-11-21 15:31:44 +0100

    x11 test

--- src/wnd/Linux/X11_Window.hpp
+++ src/wnd/Linux/X11_Window.hpp
@@ -1,4 +1,9 @@
 #pragma once
+#include <X11/Xlib.h>
+#include <X11/Xatom.h>
+#include <X11/Xutil.h>
+#include "com/Types.hpp"
+#include "com/Print.hpp"
 
 ///////////////////////////////////////////////////////////
 
@@ -6,8 +11,39 @@ namespace mini::wnd {
 
 ///////////////////////////////////////////////////////////
 
-struct X11_Window
+struct X11_Window {
+
+///////////////////////////////////////////////////////////
+    
+PRIVATE Window windowId;
+
+///////////////////////////////////////////////////////////
+
+PUBLIC X11_Window(
+    chars_t title  = "Window",
+    i32 width  = 600,
+    i32 height = 400,
+    i32 xpos   = 64,
+    i32 ypos   = 64)
 {
+    windowId = XCreateWindow(
+        0,          //display
+        0,          //paren
+        xpos,       //xpos
+        ypos,       //ypos
+        width,      //width
+        height,     //height
+        0,          //border
+        0,          //depth
+        0,          //clazz
+        0,          //visual
+        0,          //mask
+        0           //attributes
+    );
+
+}
+
+///////////////////////////////////////////////////////////
 
 };  
 
