cab0ada8e653ac4586040a2696b97ed606fda047
awwdev <aw.mail.ranked@gmail.com>
2020-11-21 18:08:29 +0100

    unix console colors

--- src/com/Print.hpp
+++ src/com/Print.hpp
@@ -21,10 +21,9 @@ enum class ConsoleColor
 
 ///////////////////////////////////////////////////////////
 
-inline void PrintColored(ConsoleColor col, auto... args)
+inline void Win32_SetColor(ConsoleColor col)
 {
-#ifdef _WIN32
-    //TODO using enum 
+    //TODO using enum
     switch(col)
     {
         case ConsoleColor::Red:      std::cout << "\x1b[91m"; break;
@@ -36,13 +35,61 @@ inline void PrintColored(ConsoleColor col, auto... args)
         case ConsoleColor::White: break;
         default: break;
     }
-#endif
+}
+
+///////////////////////////////////////////////////////////
+
+inline void X11_SetColor(ConsoleColor col)
+{
+    //TODO using enum
+    switch(col)
+    {
+        case ConsoleColor::Red:      std::cout << "\x1B[31m"; break;
+        case ConsoleColor::Green:    std::cout << "\x1B[32m"; break;
+        case ConsoleColor::Yellow:   std::cout << "\x1B[33m"; break;
+        case ConsoleColor::Blue:     std::cout << "\x1B[34m"; break;
+        case ConsoleColor::Magenta:  std::cout << "\x1B[35m"; break;
+        case ConsoleColor::Cyan:     std::cout << "\x1B[36m"; break;
+        case ConsoleColor::White: break;
+        default: break;
+    }
+}
+
+///////////////////////////////////////////////////////////
+
+inline void Win32_ResetColor()
+{
+    std::cout << "\x1b[0m";
+}
+
+///////////////////////////////////////////////////////////
+
+inline void X11_ResetColor()
+{
+    std::cout << "\033[0m";
+}
+
+///////////////////////////////////////////////////////////
+
+inline void PrintColored(ConsoleColor col, auto... args)
+{
+    #ifdef _WIN32
+    Win32_SetColor(col);
+    #endif
+
+    #ifdef __linux__
+    X11_SetColor(col);
+    #endif
 
     ((std::cout << args << ' '), ...);
 
-#ifdef _WIN32
-    std::cout << "\x1b[0m"; //reset
-#endif
+    #ifdef _WIN32
+    Win32_ResetColor();
+    #endif
+
+    #ifdef __linux__
+    X11_ResetColor();
+    #endif
 
     std::cout << '\n';
 }
