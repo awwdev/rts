a5c0c402bc9718acd7a73de711e576d089d2741c
awwdev <aw.mail.ranked@gmail.com>
2020-12-04 14:09:31 +0100

    fixed alpha blending

--- src/gpu/vuk/Wrappers/PipelineExt.hpp
+++ src/gpu/vuk/Wrappers/PipelineExt.hpp
@@ -192,8 +192,8 @@ inline auto BlendStateInfo()
         .srcColorBlendFactor  = VK_BLEND_FACTOR_SRC_ALPHA,
         .dstColorBlendFactor  = VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA, 
         .colorBlendOp         = VK_BLEND_OP_ADD,
-        .srcAlphaBlendFactor  = VK_BLEND_FACTOR_ONE,
-        .dstAlphaBlendFactor  = VK_BLEND_FACTOR_ZERO,
+        .srcAlphaBlendFactor  = VK_BLEND_FACTOR_SRC_ALPHA,
+        .dstAlphaBlendFactor  = VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA,
         .alphaBlendOp         = VK_BLEND_OP_ADD,
         .colorWriteMask       = 
             VK_COLOR_COMPONENT_R_BIT | 
